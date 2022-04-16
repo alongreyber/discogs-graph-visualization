2909662295ea069025c303b9efa0dd0195cd59c4
Alon Greyber <alon@Alons-MacBook-Pro-2.local>
2022-04-16 10:51:34 -0400

    Finished temporary mermaid rendering script

--- src/main.py
+++ src/main.py
@@ -6,7 +6,12 @@ import psycopg2.extras
 def sanitize(s):
     return re.sub(r'[^a-zA-Z0-9 ]', '', s)
 
-graph = "graph TD\n"
+allowed_roles = [
+    "Mastered By", "Mixed By", "Produced By", "Performer", "Written By", "Written-By",
+    "Producer"
+]
+
+graph = "graph LR\n"
 
 # Connect to an existing database
 conn = psycopg2.connect("dbname=discogs user=alon password=the3Qguy")
@@ -15,14 +20,23 @@ conn = psycopg2.connect("dbname=discogs user=alon password=the3Qguy")
 cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
 
 # Get release
-master_title = "Narrow Stairs"
+# master_title = "An Evening With Silk Sonic"
+master_title = "Head Hunters"
 cur.execute("SELECT * FROM master WHERE title = %s;", [master_title])
 master = cur.fetchone()
 # Add to graph
 graph += f"    {master.main_release}[{sanitize(master.title)}]\n"
 
 # Get release_artist associations
-cur.execute("SELECT * FROM release_artist WHERE release_id = %s LIMIT 10", [master.main_release])
+cur.execute(
+    """
+    SELECT * FROM release_artist
+    WHERE release_id = %s
+    AND (release_artist.role IN %s OR release_artist.role IS NULL)
+    LIMIT 20
+    """,
+    [master.main_release, tuple(allowed_roles)]
+)
 release_artists = cur.fetchall()
 for ra in release_artists:
     # Add artist node
@@ -30,12 +44,23 @@ for ra in release_artists:
     # Add connection
     if ra.role:
         graph += f"    {ra.release_id} -->|{sanitize(ra.role)}| {ra.artist_id}\n"
+    else:
+        graph += f"    {ra.release_id} --> {ra.artist_id}\n"
 
 # Use dict to deduplicate artists
 artists = { ra.artist_id : ra for ra in release_artists }
 # Get associated albums for each artist
 for a in artists.values():
-    cur.execute("SELECT release_id, title, role FROM release_artist JOIN release ON release_artist.release_id = release.id WHERE artist_id = %s LIMIT 5;", [a.artist_id])
+    cur.execute(
+        """
+        SELECT release_id, title, role
+        FROM release_artist JOIN release ON release_artist.release_id = release.id
+        WHERE artist_id = %s
+        AND (release_artist.role IN %s OR release_artist.role IS NULL)
+        LIMIT 5;
+        """,
+        [a.artist_id, tuple(allowed_roles)]
+    )
     releases = cur.fetchall()
     for r in releases:
         # Add album node
@@ -43,6 +68,8 @@ for a in artists.values():
         # Add connection
         if r.role:
             graph += f"    {r.release_id} -->|{sanitize(r.role)}| {a.artist_id}\n"
+        else:
+            graph += f"    {r.release_id} --> {a.artist_id}\n"
 
 
 print(graph)
